e3ff7db214f69fc96c92d896a5f8fb5a8d342754
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-05-30 20:42:54 +0200

    Fixed issue in Data Core data.plist - this class in not key value / unrecognised selector sent to instance

--- MyFoodChoice/ViewController.swift
+++ MyFoodChoice/ViewController.swift
@@ -12,29 +12,57 @@ class ViewController: UIViewController {
     
     var context: NSManagedObjectContext!
     
-    @IBOutlet weak var foodLabel: UILabel!
+    // Time formater
+    lazy var dateFormater: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
+    // Background Image
+    var imageView: UIImageView = {
+          let imageView = UIImageView(frame: .zero)
+          imageView.image = UIImage(named: "background.jpg")
+        imageView.contentMode = .scaleAspectFill
+          imageView.translatesAutoresizingMaskIntoConstraints = false
+          return imageView
+      }()
+    
+    @IBOutlet weak var foodsLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var lastTimeLabel: UILabel!
+    @IBOutlet weak var lastEatenLabel: UILabel!
+    @IBOutlet weak var imageName: UIImageView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var numberOfTimesLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
-    @IBOutlet weak var PickerView: UIPickerView!
     @IBOutlet weak var myChoiceImage: UIImageView!
     
-    @IBAction func eatButtonPressed(_ sender: Any) {
+    @IBAction func segmentedCtrlPressed(_ sender: Any) {
+        
     }
     
-    
-    @IBAction func rateButtonPressed(_ sender: Any) {
+    // Method for inserting data from dictionary to Interface
+    private func insertDataFrom(selectedFood food: Food) {
+        imageName.image = UIImage(data: food.imageData!)
+        foodsLabel.text = food.foods
+        typeLabel.text = food.type
+        myChoiceImage.isHidden = !(food.myChoice)
+        ratingLabel.text = "Rating: \(food.rating) / 10"
+        numberOfTimesLabel.text = "Number of times: \(food.numberOfTimes)"
+        
+        lastEatenLabel.text = "Last time eaten: \(dateFormater.string(from: food.lastEaten!))"
+        
+        // segment
+        segmentedControl.tintColor = food.tintColor as? UIColor
     }
-    
     
     // Method for extracting data from file
     private func getDataFromFile() {
         
         // Fetch
         let fetchRequest: NSFetchRequest<Food> = Food.fetchRequest()
-        fetchRequest.predicate = NSPredicate(format: "mark != nil ") // what we want to get
+        fetchRequest.predicate = NSPredicate(format: "foods != nil ") // what we want to get
         
         var records = 0
         
@@ -57,10 +85,10 @@ class ViewController: UIViewController {
             let food = NSManagedObject(entity: entity!, insertInto: context) as! Food
             
             let foodDictionary = dictionary as! [String : AnyObject]
-            food.food = foodDictionary["food"] as? String
+            food.foods = foodDictionary["foods"] as? String
             food.type = foodDictionary["type"] as? String
             food.rating = foodDictionary["rating"] as! Double
-            food.lastEated = foodDictionary["lastEated"] as? Date
+            food.lastEaten = foodDictionary["lastEaten"] as? Date
             food.myChoice = foodDictionary["myChoice"] as! Bool
             food.numberOfTimes = foodDictionary["numberOfTimes"] as! Int16
             
@@ -68,31 +96,57 @@ class ViewController: UIViewController {
             // Image
             let imageName = foodDictionary["imageName"] as? String
             let image = UIImage(named: imageName!)
-            let imageData = image?.pngData()
+            let imageData = image!.pngData()
             food.imageData = imageData
             
             
             // Tasty
-            if let tastyDictionary = foodDictionary["tasty"] as? [String: Float] {
+            if let colorDictionary = foodDictionary["tintColor"] as? [String: Float] {
                 // getting value as Array
-                food.tasty = getTasty(tastyDictionary: tastyDictionary)
+                food.tintColor = getColor(colorDictionary: colorDictionary)
     }
   }
 }
     
     // Method getTasty
-    private func getTasty(tastyDictionary: [String: Float]) -> UIColor {
-        guard let red = tastyDictionary["red"],
-              let green = tastyDictionary["green"],
-              let blue = tastyDictionary["blue"] else { return UIColor()}
+    private func getColor(colorDictionary: [String: Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+              let green = colorDictionary["green"],
+              let blue = colorDictionary["blue"] else { return UIColor()}
         return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Background
+        view.insertSubview(imageView, at: 0)
+              NSLayoutConstraint.activate([
+                  imageView.topAnchor.constraint(equalTo: view.topAnchor),
+                  imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                  imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                  imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+              ])
+        
+        //Segmented Control Title Color
+        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.normal)
+        
         // getting data from data.plist
         getDataFromFile()   // Can be implemented by USER DEFAULTS
+        
+        let fetchRequest: NSFetchRequest<Food> = Food.fetchRequest()
+        let foods = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "foods == %@", foods!) // what we want to get
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            let selectedFood = results.first
+            insertDataFrom(selectedFood: selectedFood!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
     }
 
 
