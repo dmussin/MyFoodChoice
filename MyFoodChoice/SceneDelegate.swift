76183b480875a1b14ca44ed8daf327faf7e07375
Daniyar Mussin <45901913+stormy377@users.noreply.github.com>
2021-05-30 14:24:29 +0200

    FoodChoice App HID data.plist CoreData

--- MyFoodChoice/SceneDelegate.swift
+++ MyFoodChoice/SceneDelegate.swift
@@ -13,10 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // Getting to the view controller to give a text
+        let vc = window?.rootViewController as! ViewController
+        // creating a context
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        vc.context = context
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- MyFoodChoice/ViewController.swift
+++ MyFoodChoice/ViewController.swift
@@ -6,12 +6,93 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    var context: NSManagedObjectContext!
+    
+    @IBOutlet weak var foodLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lastTimeLabel: UILabel!
+    @IBOutlet weak var numberOfTimesLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var PickerView: UIPickerView!
+    @IBOutlet weak var myChoiceImage: UIImageView!
+    
+    @IBAction func eatButtonPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func rateButtonPressed(_ sender: Any) {
+    }
+    
+    
+    // Method for extracting data from file
+    private func getDataFromFile() {
+        
+        // Fetch
+        let fetchRequest: NSFetchRequest<Food> = Food.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil ") // what we want to get
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data There Already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        // how many records we do have in RECORDS
+        guard records == 0 else { return }
+        
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+              let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+        
+    // itteration and placing to core data as an objects
+        for dictionary in dataArray {
+            let entity = NSEntityDescription.entity(forEntityName: "Food", in: context)
+            let food = NSManagedObject(entity: entity!, insertInto: context) as! Food
+            
+            let foodDictionary = dictionary as! [String : AnyObject]
+            food.food = foodDictionary["food"] as? String
+            food.type = foodDictionary["type"] as? String
+            food.rating = foodDictionary["rating"] as! Double
+            food.lastEated = foodDictionary["lastEated"] as? Date
+            food.myChoice = foodDictionary["myChoice"] as! Bool
+            food.numberOfTimes = foodDictionary["numberOfTimes"] as! Int16
+            
+            
+            // Image
+            let imageName = foodDictionary["imageName"] as? String
+            let image = UIImage(named: imageName!)
+            let imageData = image?.pngData()
+            food.imageData = imageData
+            
+            
+            // Tasty
+            if let tastyDictionary = foodDictionary["tasty"] as? [String: Float] {
+                // getting value as Array
+                food.tasty = getTasty(tastyDictionary: tastyDictionary)
+    }
+  }
+}
+    
+    // Method getTasty
+    private func getTasty(tastyDictionary: [String: Float]) -> UIColor {
+        guard let red = tastyDictionary["red"],
+              let green = tastyDictionary["green"],
+              let blue = tastyDictionary["blue"] else { return UIColor()}
+        return UIColor(red: CGFloat(red / 255), green: CGFloat(green / 255), blue: CGFloat(blue / 255), alpha: 1.0)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // getting data from data.plist
+        getDataFromFile()   // Can be implemented by USER DEFAULTS
     }
 
 
